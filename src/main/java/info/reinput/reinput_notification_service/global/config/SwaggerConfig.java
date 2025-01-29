a9e5e61e5a1146fc72db8a05d6f2b0492e50f293
MinhoJJang <ahchjang@naver.com>
2025-01-29 19:04:33 +0900

    feat: configure Swagger OpenAPI documentation for notification service

---
+++ src/main/java/info/reinput/reinput_notification_service/global/config/SwaggerConfig.java
@@ -0,0 +1,64 @@
+package info.reinput.reinput_notification_service.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.customizers.OperationCustomizer;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .servers(createServers())
+                .info(createApiInfo())
+                .components(new Components()
+                        .addSecuritySchemes("bearer-jwt", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+
+    @Bean
+    public OperationCustomizer operationCustomizer() {
+        return (operation, handlerMethod) -> {
+            if (operation.getParameters() != null) {
+                operation.getParameters().removeIf(param ->
+                        "X-User-Id".equalsIgnoreCase(param.getName()));
+            }
+            return operation;
+        };
+    }
+
+    private List<Server> createServers() {
+        return List.of(
+                new Server().url("").description("Direct"),
+                new Server().url("/notification").description("Gateway")
+        );
+    }
+
+    private Info createApiInfo() {
+        return new Info()
+                .title("Reinput Notification Service")
+                .version("v0.0.1")
+                .description("""
+                        Reinput Notification Service
+                        """);
+    }
+}
